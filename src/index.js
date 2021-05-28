02f298f0b68bee6d8df719246b3ccaefcc07f021
Pavel-Liteiniy <paulsev96@gmail.com>
2021-05-28 18:08:02 +0300

    Выносит объявление api на уровень выше, в index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import './sass/base.scss'
 import './sass/wrapper.scss'
 
 import App from './components/app'
+import Api from './service/api'
+
+const END_POINT = `https://interview-test-task-api.herokuapp.com/pages?page=`
+const PATH = 1
+
+const api = new Api( END_POINT, PATH )
 
 ReactDOM.render(
-  <App />,
+  <App api={ api } />,
   document.getElementById( 'root' )
 )
