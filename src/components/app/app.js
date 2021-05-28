c19a22263a915fff976c90d35ca1403d6cf5a144
Pavel-Liteiniy <paulsev96@gmail.com>
2021-05-28 18:13:36 +0300

    Выносит state в компонент App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Api from '../../service/api'
 import Relevant from '../relevant'
 
-const END_POINT = `https://603e38c548171b0017b2ecf7.mockapi.io`
-const PATH = `homes`
+import Context from '../context';
 
-const App = () => {
-  const api = new Api( END_POINT, PATH )
+const App = ( { api } ) => {
+  const [ error, setError ] = useState( null )
+  const [ isLoaded, setIsLoaded ] = useState( false )
+  const [ homes, setHomes ] = useState( [] )
+  const [ pageNumber, setPageNumber ] = useState( null )
+
+
+  const getHomes = () => {
+    api.getHomes()
+      .then( ( { homes, pageNumber } ) => {
+        setIsLoaded( true )
+        setHomes( ( prevHomes ) => [ ...prevHomes, ...homes ] )
+        setPageNumber( pageNumber )
+      } )
+      .catch( error => {
+        setIsLoaded( true )
+        setError( error )
+      } )
+  }
+
+  useEffect( getHomes, [ api ] )
 
   return (
     <>
       <h1 className="visually-hidden">Search for real estate purchase</h1>
-      <Router>
-        <Route path="/" render={ () => <Relevant api={ api } /> } exact />
-        <Route path="/details/:id" render={ ( { match } ) => {
-          return (
-            <div className="wrapper"><h2>{ `There should be detailed information about the house - id ${match.params.id}` }</h2></div>
-          )
-        } } />
-      </Router>
+      <Context.Provider value={ { homes, pageNumber, isLoaded, error, getHomes } }>
+        <Router>
+          <Route path="/" render={ () => <Relevant /> } exact />
+          <Route path="/details/:id" render={ ( { match } ) => {
+            return (
+              <div className="wrapper"><h2>{ `There should be detailed information about the house - id ${match.params.id}` }</h2></div>
+            )
+          } } />
+        </Router>
+      </Context.Provider>
     </>
   )
 }
